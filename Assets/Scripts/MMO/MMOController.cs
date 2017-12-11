e3348e1e3100aa0c48978adf51b47b54a8437fc1
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-12-11 16:04:39 +0900

    add mmo ui .login panel.

--- Assets/Scripts/MMO/MMOController.cs
+++ Assets/Scripts/MMO/MMOController.cs
@@ -13,19 +13,24 @@ namespace MMO
 		SimpleRpgAnimator mRpgPlayerAnimator;
 		SimpleRpgPlayerController mRpgPlayerController;
 		public GameObject playerPrefab;
-		Dictionary<int,GameObject> otherPlayers;
+		public SimpleRpgCamera rpgCamera;
+
+		Dictionary<int,GameObject> mOtherPlayers;
+		List<int> mOtherPlayerIds;
 		int mPlayerId;
 		Vector3 mPrePosition;
 		Vector3 mPreForward;
 		string mPreAction;
 		float mPreSpeed;
 
+
 		void Start ()
 		{
 			mRpgPlayerAnimator = player.GetComponentInChildren<SimpleRpgAnimator> (true);
 			mRpgPlayerController = player.GetComponentInChildren<SimpleRpgPlayerController> (true);
-			otherPlayers = new Dictionary<int, GameObject> ();
-			client.Connect (targetIp, NetConstant.LISTENE_PORT, OnConnected,OnRecievePlayerInfo, OnRecieveMessage);
+			mOtherPlayers = new Dictionary<int, GameObject> ();
+			mOtherPlayerIds = new List<int> ();
+//			client.Connect (targetIp, NetConstant.LISTENE_PORT, OnConnected,OnRecievePlayerInfo, OnRecieveMessage);
 		}
 
 		void Update ()
@@ -39,6 +44,10 @@ namespace MMO
 					SendMessage (player);
 				}
 			}
+		}
+
+		public void Connect(string ip,int port){
+			client.Connect (ip, port, OnConnected,OnRecievePlayerInfo, OnRecieveMessage);
 		}
 
 		public void SendMessage(Transform player){
@@ -59,6 +68,9 @@ namespace MMO
 		{
 			PlayerInfo playerInfo = msg.ReadMessage<PlayerInfo> ();
 			mPlayerId = playerInfo.playerId;
+			rpgCamera.enabled = true;
+			player.gameObject.SetActive (true);
+
 //			PSMeshRendererUpdater updater = BodyEffects.Instance.renderers [16];
 //			GameObject effect = Instantiate (updater.gameObject);
 //			effect.transform.SetParent (player);
@@ -72,16 +84,29 @@ namespace MMO
 		{
 			TransferData playerHandle = msg.ReadMessage<TransferData> ();
 //			Debug.Log (JsonUtility.ToJson (playerHandle));
+			HashSet<int> activedPlayerIds = new HashSet<int>();
 			for (int i = 0; i < playerHandle.playerDatas.Length; i++) {
 				if (playerHandle.playerDatas [i].playerId != mPlayerId) {
-					if (!otherPlayers.ContainsKey (playerHandle.playerDatas [i].playerId)) {
-						otherPlayers.Add (playerHandle.playerDatas [i].playerId, Instantiate (playerPrefab));
-						otherPlayers [playerHandle.playerDatas [i].playerId].SetActive (true);
+					if (!mOtherPlayers.ContainsKey (playerHandle.playerDatas [i].playerId)) {
+						mOtherPlayers.Add (playerHandle.playerDatas [i].playerId, Instantiate (playerPrefab));
+						mOtherPlayers [playerHandle.playerDatas [i].playerId].SetActive (true);
+						mOtherPlayerIds.Add (playerHandle.playerDatas [i].playerId);
 					}
-					otherPlayers [playerHandle.playerDatas [i].playerId].transform.position = playerHandle.playerDatas [i].playerPosition;
-					otherPlayers [playerHandle.playerDatas [i].playerId].transform.forward = playerHandle.playerDatas [i].playerForward;
-					otherPlayers [playerHandle.playerDatas [i].playerId].GetComponent<SimpleRpgAnimator>().Action = playerHandle.playerDatas [i].action;
-					otherPlayers [playerHandle.playerDatas [i].playerId].GetComponent<SimpleRpgAnimator> ().SetSpeed (playerHandle.playerDatas [i].animSpeed);
+					activedPlayerIds.Add (playerHandle.playerDatas [i].playerId );
+					mOtherPlayers [playerHandle.playerDatas [i].playerId].transform.position = playerHandle.playerDatas [i].playerPosition;
+					mOtherPlayers [playerHandle.playerDatas [i].playerId].transform.forward = playerHandle.playerDatas [i].playerForward;
+					mOtherPlayers [playerHandle.playerDatas [i].playerId].GetComponent<SimpleRpgAnimator>().Action = playerHandle.playerDatas [i].action;
+					mOtherPlayers [playerHandle.playerDatas [i].playerId].GetComponent<SimpleRpgAnimator> ().SetSpeed (playerHandle.playerDatas [i].animSpeed);
+				}
+			}
+
+			for(int i=0;i<mOtherPlayerIds.Count;i++){
+				if (!activedPlayerIds.Contains (mOtherPlayerIds [i])) {
+					int id = mOtherPlayerIds [i];
+					Destroy (mOtherPlayers [id]);
+					mOtherPlayerIds.Remove (id);
+					mOtherPlayers.Remove (id);
+					i--;
 				}
 			}
 		}
---
+++ Assets/Scripts/MMO/MMOUIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MMO
+{
+	public class MMOUIController : SingleMonoBehaviour<MMOUIController>
+	{
+
+		public const string TARGET_IP = "TARGET_IP";
+		public const string TARGET_PORT = "TARGET_PORT";
+
+		public InputField input_ip;
+		public InputField input_port;
+		public Button btn_connect;
+		public GameObject uiRoot;
+
+		protected override void Awake ()
+		{
+			base.Awake ();
+			input_ip.text = PlayerPrefs.GetString (TARGET_IP);
+			input_port.text = PlayerPrefs.GetString (TARGET_PORT);
+			btn_connect.onClick.AddListener (()=>{
+				MMOController.Instance.Connect(input_ip.text.Trim(),int.Parse(input_port.text));
+				PlayerPrefs.SetString(TARGET_IP,input_ip.text);
+				PlayerPrefs.SetString(TARGET_PORT,input_port.text);
+				PlayerPrefs.Save();
+				uiRoot.SetActive(false);
+			});
+		}
+
+	}
+}
