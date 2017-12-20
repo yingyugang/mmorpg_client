6807b8fc1a429d8f070336c3ffe56e7d9a3391a8
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-12-20 15:14:54 +0900

    ソウースコードの結構を最適化する。

---
+++ Assets/Scripts/status_network/skills/SkillBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MMO
+{
+	public class SkillBase
+	{
+
+		public float coolDown = 5f;
+		float mNextActiveTime;
+
+		public virtual void OnAwake ()
+		{
+		
+		}
+
+		public virtual void OnEnable ()
+		{
+
+		}
+
+		public virtual bool IsUseAble ()
+		{
+			return mNextActiveTime < Time.time;
+		}
+
+		public virtual bool Play ()
+		{
+			bool playAble = IsUseAble ();
+			if(playAble){
+				OnActive ();
+			}
+			return playAble;
+		}
+
+		protected virtual void OnActive(){
+			mNextActiveTime = Time.time + coolDown;
+		}
+
+	}
+}
---
+++ Assets/Scripts/status_network/skills/SkillDefault.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MMO
+{
+	public class SkillDefault : SkillBase
+	{
+
+		public override bool Play ()
+		{
+			return base.Play ();
+		}
+
+		protected override void OnActive ()
+		{
+			base.OnActive ();
+			//DO somthing.
+		}
+
+	}
+}
