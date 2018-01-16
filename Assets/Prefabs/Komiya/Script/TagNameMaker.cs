89f18a21cc9d1a7df37a1f9c941a4ad4ba546e7d
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2018-01-16 15:03:43 +0900

    fix build error.

--- Assets/Prefabs/Komiya/Script/TagNameMaker.cs
+++ Assets/Prefabs/Komiya/Script/TagNameMaker.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace izvr
 {
 	public class TagNameMaker : MonoBehaviour {
 
 		public static void addTag(string tagname) {
+			#if UNITY_EDITOR
 			UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
 			if ((asset != null) && (asset.Length > 0)) {
 				SerializedObject so = new SerializedObject(asset[0]);
@@ -24,6 +27,7 @@ namespace izvr
 				so.ApplyModifiedProperties();
 				so.Update();
 			}
+			#endif
 		}
 	}
 }
