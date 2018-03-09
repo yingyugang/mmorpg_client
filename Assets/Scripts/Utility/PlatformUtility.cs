6b0329fe470c7787f1af196ce359436aae8a693c
应彧刚 <>
2018-03-09 22:22:43 +0900

    platform utility.

---
+++ Assets/Scripts/Utility/PlatformUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformUtility : MonoBehaviour {
+
+	void Awake(){
+		#if UNITY_IOS || UNITY_ANDROID
+		gameObject.SetActive (false);
+		#endif
+	}
+
+}
