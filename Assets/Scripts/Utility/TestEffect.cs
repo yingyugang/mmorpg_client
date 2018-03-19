894b37c5c65bae3d0d45f3f07ce9ebaa3afb406b
应彧刚 <>
2018-03-20 08:22:27 +0900

    amend scene and add test motion.

---
+++ Assets/Scripts/Utility/TestEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEffect : MonoBehaviour {
+
+	public GameObject effectPrefab;
+	public float delay = 0.2f;
+	public float speed = 10;
+	public Transform trans;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (_Spawn());
+	}
+	
+	IEnumerator _Spawn(){
+		while(true){
+			yield return new WaitForSeconds (delay);
+			GameObject go = Instantiate (effectPrefab);
+			go.transform.position = transform.position;
+			StartCoroutine (_Fly(go));
+		}
+	}
+
+	IEnumerator _Fly(GameObject go){
+		float t = 0;
+		while(t<1){
+			t += Time.deltaTime / 10;
+			go.transform.position += Time.deltaTime * speed * trans.forward;
+			yield return null;
+		}
+		Destroy (go);
+	}
+}
