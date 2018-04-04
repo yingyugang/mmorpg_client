edc5a919506d1634c84acfb4ae3b3b4bfa1ab109
应彧刚 <>
2018-04-04 16:43:08 +0900

    small utility to close the shadow.

---
+++ Assets/Scripts/status_network/utility/SkilledMeshShadowUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SkilledMeshShadowUtility : MonoBehaviour {
+
+	public bool load;
+
+	void Update () {
+		if (load) {
+			load = false;
+			SkinnedMeshRenderer[] rsr = gameObject.GetComponentsInChildren<SkinnedMeshRenderer> (true);
+			for(int i=0;i<rsr.Length;i++){
+				rsr [i].receiveShadows = false;
+				rsr [i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+			}
+		}
+	}
+}
