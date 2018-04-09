0b9e27c493d487a8ee506fe7e9c6279a6e73e27d
应彧刚 <>
2018-04-09 17:22:41 +0900

    add the bottom skill button.

---
+++ Assets/Scripts/status_network/ui/MainInterface/EquipmentButtonGroup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace MMO
+{
+	public class EquipmentButtonGroup : MonoBehaviour
+	{
+
+		public Button btn_arrow;
+		public GridLayoutGroup grid_equipment;
+		public List<Button> btn_items;
+		const float moveDuration = 0.2f;
+		const float startY = -57;
+		const float endY = 57;
+		void Awake(){
+			btn_arrow.onClick.AddListener (()=>{
+				if(!mIsShowed)
+					ShowEquipments();
+				else
+					HideEquipments();
+			});
+		}
+
+		bool mIsShowed;
+		void ShowEquipments(){
+				mIsShowed = true;
+			btn_arrow.transform.localEulerAngles = new Vector3 (0,0,180);
+				grid_equipment.GetComponent<RectTransform> ().DOAnchorPosY (endY,moveDuration);
+		}
+
+		void HideEquipments(){
+				mIsShowed = false;
+				btn_arrow.transform.localEulerAngles = new Vector3 (0,0,0);
+				grid_equipment.GetComponent<RectTransform> ().DOAnchorPosY (startY,moveDuration);
+		}
+
+	}
+}
